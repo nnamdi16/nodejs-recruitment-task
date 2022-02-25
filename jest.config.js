2da20f096e7c5ee0f40e6c4cc0039f7f3d1765bd
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2022-02-25 06:00:11 +0100

    feat(develop): jest setup

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    verbose: true,
+    roots: ['src'],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    coverageReporters: [
+        "json-summary",
+        "text",
+        "lcov",
+        "clover"
+    ]
+
+
+};
