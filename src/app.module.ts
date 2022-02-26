8b69387cc12ba5cb92eb7122ac8f16d57dbc8af4
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2022-02-26 14:04:21 +0100

    feat(main): db setup  and connection to postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as Joi from 'joi';
+import { DatabaseModule } from './infrastructure/database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({})],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HEROKU_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_HEROKU_USER: Joi.string().required(),
+        POSTGRES_HEROKU_PASSWORD: Joi.string().required(),
+        POSTGRES_HEROKU_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
