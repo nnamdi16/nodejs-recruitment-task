d060727e56527072afa5fc1f1a7dc0f3f46fb6b4
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2022-03-02 08:31:02 +0100

    feat(main-user-service):Implemented the endpoint to create movies

--- movie/src/app/controllers/movie.controller.ts
+++ movie/src/app/controllers/movie.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { MovieService } from '../../domain/services/movie.service';
+import { CreateMovieDto } from '../../domain/dto/createMovie.dto';
 
 @Controller('movie')
-export class MoviesController {}
+@ApiTags('movie')
+export class MoviesController {
+  constructor(private readonly movieService: MovieService) {}
+  @Get('create/:title')
+  public async post(@Param('title') title: string): Promise<any> {
+    const movieDto: CreateMovieDto = {
+      title,
+    };
+    return await this.movieService.create(movieDto);
+  }
+}
