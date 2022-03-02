8b9985b0778cce380d70776f44f2e961ce697a8d
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2022-03-02 11:09:55 +0100

    feat(main-user-service):Created BaseEntity

---
+++ movie/src/infrastructure/database/PostgresDb/entities/base.entity.ts
@@ -0,0 +1,38 @@
+import {
+  BeforeUpdate,
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export abstract class BaseEntity {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ type: 'boolean', default: true })
+  isActive: boolean;
+
+  @Column({ type: 'boolean', default: false })
+  isArchived: boolean;
+
+  @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+  createdDate: Date;
+
+  @Column({ type: 'varchar', length: 300, nullable: true })
+  createdBy: string;
+
+  @UpdateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+  updatedDate: Date;
+
+  @Column({ type: 'varchar', length: 300, nullable: true })
+  updatedBy: string;
+
+  @Column({ type: 'varchar', length: 300, nullable: true })
+  Comment: string;
+
+  @BeforeUpdate()
+  updateTimestamp() {
+    this.updatedDate = new Date();
+  }
+}
