80d8f070a0bde43f72703d3d28f6a5d38722a6fa
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2022-03-02 11:20:47 +0100

    feat(main-user-service): mocked movie entity

---
+++ movie/src/test/__mocks__/movie.mock.ts
@@ -0,0 +1,22 @@
+import MovieEntity from '../../infrastructure/database/PostgresDb/entities/movie.entity';
+
+const mockedMovie: MovieEntity = {
+  title: 'Guardians of the Galaxy Vol. 2',
+  released: '2017',
+  genre: 'Action, Adventure, Comedy',
+  director: 'James Gunn',
+  userId: '2',
+  id: 1,
+  isActive: false,
+  isArchived: false,
+  createdDate: new Date(),
+  createdBy: '',
+  updatedDate: undefined,
+  updatedBy: '',
+  Comment: '',
+  updateTimestamp: function (): void {
+    this.updatedDate = new Date();
+  },
+};
+
+export default mockedMovie;
