9e1e65e15f3ea37d5768b594b84a5df9cd9cc1d9
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2022-03-02 08:31:40 +0100

    feat(main-user-service): injected dependencies required in movies services

--- movie/src/app/modules/movie.module.ts
+++ movie/src/app/modules/movie.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { MoviesController } from '../controllers/movie.controller';
+import { OMDBService } from '../../domain/services/omdb.service';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
+import MovieEntity from '../../infrastructure/database/PostgresDb/entities/movie.entity';
+import { MovieService } from '../../domain/services/movie.service';
+
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([])
-  ]
+  imports: [TypeOrmModule.forFeature([MovieEntity]), ConfigModule, HttpModule],
+  controllers: [MoviesController],
+  providers: [OMDBService, MovieService],
+  exports: [OMDBService],
 })
 export class MovieModule {}
