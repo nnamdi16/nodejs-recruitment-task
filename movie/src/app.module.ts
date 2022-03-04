84ea3420d0a5383c70398d8257a84f742afc7b68
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2022-03-04 10:06:22 +0100

    feat(main-user-service): Added validation for JWT_SECRET and JWT_EXPIRATION_TIME

--- movie/src/app.module.ts
+++ movie/src/app.module.ts
@@ -16,6 +16,8 @@ import { MovieModule } from './app/modules/movie.module';
         POSTGRES_PASSWORD: Joi.string().required(),
         POSTGRES_DB: Joi.string().required(),
         PORT: Joi.number(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION_TIME: Joi.string().required(),
       }),
     }),
     DatabaseModule,
