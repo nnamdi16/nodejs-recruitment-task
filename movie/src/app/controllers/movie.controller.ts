f64c4349e46b28b3b08b5c5042f4e32bdad84ed2
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2022-03-04 10:08:36 +0100

    feat(main-user-service):Implemented endpoints to create and fetch movies

--- movie/src/app/controllers/movie.controller.ts
+++ movie/src/app/controllers/movie.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from './../middlewares/guards/jwt.guard';
+import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { MovieService } from '../../domain/services/movie.service';
 import { CreateMovieDto } from '../../domain/dto/createMovie.dto';
 
@@ -8,10 +9,22 @@ import { CreateMovieDto } from '../../domain/dto/createMovie.dto';
 export class MoviesController {
   constructor(private readonly movieService: MovieService) {}
   @Get('create/:title')
-  public async post(@Param('title') title: string): Promise<any> {
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  public async createMovie(
+    @Param('title') title: string,
+    @Req() req,
+  ): Promise<any> {
     const movieDto: CreateMovieDto = {
       title,
     };
-    return await this.movieService.create(movieDto);
+    return await this.movieService.create(movieDto, req?.user);
+  }
+
+  @Get('/movies')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  public async fetchMovies(@Req() req): Promise<any> {
+    return await this.movieService.findAllByAuthorisedUser(req.user);
   }
 }
