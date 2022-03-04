1cf5600e4aa20ec810648bd82de203b70873686a
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2022-03-04 10:11:14 +0100

    feat(main-user-service):Injected dependencies such as MovieEntity and other services

--- movie/src/app/modules/movie.module.ts
+++ movie/src/app/modules/movie.module.ts
@@ -3,15 +3,33 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { MoviesController } from '../controllers/movie.controller';
 import { OMDBService } from '../../domain/services/omdb.service';
-import { ConfigModule } from '@nestjs/config';
-import { HttpModule } from '@nestjs/axios';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import MovieEntity from '../../infrastructure/database/PostgresDb/entities/movie.entity';
 import { MovieService } from '../../domain/services/movie.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from '../middlewares/guards/jwt.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MovieEntity]), ConfigModule, HttpModule],
+  imports: [
+    TypeOrmModule.forFeature([MovieEntity]),
+    PassportModule,
+    ConfigModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: `${configService.get('JWT_EXPIRATION_TIME')}`,
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    PassportModule.register({ defaultStrategy: 'bearer' }),
+  ],
   controllers: [MoviesController],
-  providers: [OMDBService, MovieService],
-  exports: [OMDBService],
+  providers: [OMDBService, MovieService, JwtStrategy],
+  exports: [OMDBService, MovieService, JwtStrategy],
 })
 export class MovieModule {}
